2584af2a3ed57957b7b46d9bc128d77137a427b9
lupesms97 <lupesms97@gmail.com>
2023-06-09 11:25:20 -0300

    fixing: corrigindo nomeclatura.

--- src/funcaoRecursiva/FibonnaciRecursiva.java
+++ src/funcaoRecursiva/FibonnaciRecursiva.java
@@ -8,14 +8,14 @@ public class FibonnaciRecursiva {
 
     public static List<Integer> fiboList = new ArrayList<>();
 
-    public static void showFiboList(int x) {
+    public static void showFibo(int x) {
         System.out.println(fiboList);
         System.out.printf("Complex: O valor correspondente ao item na posição %d é o %d", x, fiboList.get(x-1));
         System.out.println();
-        System.out.printf("Simple: O valor correspondente ao item na posição %d é o %d", x, fiboFunctionSimple(x-1));
+        System.out.printf("Simple: O valor correspondente ao item na posição %d é o %d", x, fiboRecursiveSimple(x-1));
     }
 
-    public static List<Integer> fiboFunctionComplex(int x) {
+    public static List<Integer> fiboRecursiveComplex(int x) {
         if (x <= 0) {
             return fiboList;
         }
@@ -31,17 +31,17 @@ public class FibonnaciRecursiva {
             return fiboList;
         }
 
-        fiboFunctionComplex(x - 1);
+        fiboRecursiveComplex(x - 1);
         int fiboNum = fiboList.get(x - 2) + fiboList.get(x - 3);
         fiboList.add(fiboNum);
 
         return fiboList;
     }
 
-    public static int fiboFunctionSimple(int x){
+    public static int fiboRecursiveSimple(int x){
         if(x <= 1 )return x;
 
-        return  fiboFunctionSimple(x-1) + fiboFunctionSimple(x-2);
+        return  fiboRecursiveSimple(x-1) + fiboRecursiveSimple(x-2);
 
     }
 
