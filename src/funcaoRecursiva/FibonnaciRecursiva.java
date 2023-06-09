9a99289ea460c8881b807fc4e2136d8a10a8e904
lupesms97 <lupesms97@gmail.com>
2023-06-09 11:07:39 -0300

    feat: criando função fibonnaci complexa;

---
+++ src/funcaoRecursiva/FibonnaciRecursiva.java
@@ -0,0 +1,44 @@
+package funcaoRecursiva;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FibonnaciRecursiva {
+
+    public static List<Integer> fiboList = new ArrayList<>();
+
+    public static void showFiboList(int x) {
+        System.out.println(fiboList);
+        System.out.printf("Complex: O valor correspondente ao item na posição %d é o %d", x, fiboList.get(x-1));
+
+    }
+
+    public static List<Integer> fiboFunctionComplex(int x) {
+        if (x <= 0) {
+            return fiboList;
+        }
+
+        if (x == 1) {
+            fiboList.add(0);
+            return fiboList;
+        }
+
+        if (x == 2) {
+            fiboList.add(0);
+            fiboList.add(1);
+            return fiboList;
+        }
+
+        fiboFunctionComplex(x - 1);
+        int fiboNum = fiboList.get(x - 2) + fiboList.get(x - 3);
+        fiboList.add(fiboNum);
+
+        return fiboList;
+    }
+
+
+
+
+
+}
